d0654f26c8f95ee59d5f9b95f36d382d05160227
Asperi137 <jerome_chaput@hotmail.fr>
2022-10-25 16:19:46 +0200

    pourtest

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 
 const app = express()
-
-app.use((req, res) => {
-  res.json({ message: 'requete ok' })
+app.use((req, res, next) => {
+  console.log('req recu')
+  next()
 })
 
+app.use((req, res, next) => {
+  res.status(201)
+  next()
+})
+
+app.use((req, res, next) => {
+  res.json({ message: 'requete ok' })
+  next()
+})
+
+app.use((req, res) => {
+  console.log('réponse envoyer avec succes')
+})
 module.exports = app
