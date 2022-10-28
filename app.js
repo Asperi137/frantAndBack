21212f76420e85938b05b734cb9355daef6f481f
Asperi137 <jerome_chaput@hotmail.fr>
2022-10-28 10:07:17 +0200

    pour garder une trace sur mon git

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
+const path = require('path')
 
 const app = express()
+
+mongoose
+  .connect(
+    'mongodb+srv://user:root@atlascluster.bssyxn0.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+app.use(express.json())
+
 app.use((req, res, next) => {
-  console.log('req recu')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
   next()
 })
 
-app.use((req, res, next) => {
-  res.status(201)
-  next()
-})
+app.use('/api/stuff', stuffRoutes)
+app.use('/api/user', userRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
-app.use((req, res, next) => {
-  res.json({ message: 'requete ok' })
-  next()
-})
-
-app.use((req, res) => {
-  console.log('réponse envoyer avec succes')
-})
 module.exports = app
